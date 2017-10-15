73c0a99a1daeddefb91e7916b26e9685778dedee
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2017-10-15 22:06:08 +1300

    Configured the switch in the management network to enable communication between the two hosts and the controller.
    The MAC address convention was also refined.

--- app/mininet/basic_network.py
+++ app/mininet/basic_network.py
@@ -28,25 +28,45 @@ __status__ = "Development"
 
 # Explicitly define the names and other details of each node in our
 # test network.
+#
+# MAC address convention. Note that FF is not used as a value to avoid
+# the possibility of forming the broadcast MAC address.
+#
+# 00:<network>:<host type>:<switch number>:<connected
+# host type>:<host number>
+#
+# network: 00 (data plane), 11 (control plane), 22 (management)
+# host type: 00 ('normal' host), 11 (controller), 22 (switch) (used to
+#            indicate what kind of host this host is)
+# switch number: 00-FE (used to uniquely identify switches, this is set
+#                to 00 when host device type is not 22)
+# connected host type: 00 ('normal' host), 11 (controller),
+#                      22 (switch) (indicates what kind of host this
+#                      host is connected to)
+# host number: 00-FE (used to uniquely identify a host when the host
+#              device type is not 22, else it indicates what host is
+#              connected to this port)
 _NODES = {"mgmts1": {"name": "mgmts1",
-                     "mgmt_mac_c0": "00:00:00:01:cc:01",
-                     "mgmt_mac_h1": "00:00:00:01:00:11",
-                     "mgmt_mac_h2": "00:00:00:01:00:12"},
-          "dps1": {"name": "dps1", "cp_mac": "00:00:00:02:cc:50",
-                   "cp_ip": "192.168.0.50",
-                   "dp_mac_h1": "00:00:00:00:11:51",
-                   "dp_mac_h2": "00:00:00:00:12:52", "inband": False,
+                     "mgmt_mac_c0": "00:22:22:01:11:01",
+                     "mgmt_mac_h1": "00:22:22:01:01:11",
+                     "mgmt_mac_h2": "00:22:22:01:01:12"},
+          "dps1": {"name": "dps1", "cp_mac": "00:11:22:01:11:01",
+                   "cp_ip": "192.168.0.50/24",
+                   "dp_mac_h1": "00:00:22:01:00:11",
+                   "dp_mac_h2": "00:00:22:01:00:12", "inband": False,
                    "protocols": "OpenFlow13"},
-          "controller": {"name": "c0", "cp_mac": "00:00:00:02:cc:01",
-                         "cp_ip": "192.168.0.1", "of_port": 6653,
-                         "mgmt_mac": "00:00:00:01:cc:01",
-                         "mgmt_ip": "172.16.0.1"},
-          "host1": {"name": "h1", "mgmt_mac": "00:00:00:01:00:11",
-                    "mgmt_ip": "172.16.0.11",
-                    "dp_mac": "00:00:00:00:00:11", "dp_ip": "10.0.0.11"},
-          "host2": {"name": "h2", "mgmt_mac": "00:00:00:01:00:12",
-                    "mgmt_ip": "172.16.0.12",
-                    "dp_mac": "00:00:00:00:00:12", "dp_ip": "10.0.0.12"}}
+          "controller": {"name": "c0", "cp_mac": "00:11:11:00:22:01",
+                         "cp_ip": "192.168.0.1/24", "of_port": 6653,
+                         "mgmt_mac": "00:22:11:00:22:01",
+                         "mgmt_ip": "10.0.0.1/16"},
+          "host1": {"name": "h1", "mgmt_mac": "00:22:00:00:22:11",
+                    "mgmt_ip": "10.0.1.11/16",
+                    "dp_mac": "00:00:00:00:22:11",
+                    "dp_ip": "172.16.0.11/24"},
+          "host2": {"name": "h2", "mgmt_mac": "00:22:00:00:22:12",
+                    "mgmt_ip": "10.0.1.12/16",
+                    "dp_mac": "00:00:00:00:22:12",
+                    "dp_ip": "172.16.0.12/24"}}
 
 
 def get_node_configs():
@@ -70,10 +90,12 @@ class BasicNetwork(Topo):
         mgmts1_conf, dps1_conf, c0_conf, h1_conf, h2_conf = \
             get_node_configs()
 
-        # Create a switch that will connect the control plane together.
+        # Create a switch that will connect the management network
+        # together (currently 10.0.0.0/16).
         self.addSwitch(mgmts1_conf["name"])
 
-        # Create an OpenFlow switch running OVS
+        # Create an OpenFlow switch running OVS for the data plane
+        # network (currently 172.16.0.0/24).
         dps1_opts = {"inband": dps1_conf["inband"],
                      "protocols": dps1_conf["protocols"]}
         self.addSwitch(dps1_conf["name"], opts=dps1_opts)
@@ -86,8 +108,13 @@ class BasicNetwork(Topo):
         self.addHost(h2_conf["name"])
 
 
-def configure_links(net):
+def _configure_links(net):
     """Configure the links and interfaces in the network.
+
+    Note: Port 0 (i.e. eth0) on an Open vSwitch switch is not part of
+    the switch bridge used to support communication between multiple
+    hosts. Therefore switch port 0 is only used to connect interfaces
+    in an OpenFlow controller-switch relationship.
 
     :param net: A Mininet object with a virtual network.
     """
@@ -105,7 +132,7 @@ def configure_links(net):
                    addr1=c0_conf["cp_mac"],
                    addr2=dps1_conf["cp_mac"])
     c0.setIP(c0_conf["cp_ip"], intf=cp_link.intf1)
-    c0_if_mgmt = Link(c0, mgmts1, port1=1, port2=0,
+    c0_if_mgmt = Link(c0, mgmts1, port1=1, port2=1,
                       addr1=c0_conf["mgmt_mac"],
                       addr2=mgmts1_conf["mgmt_mac_c0"]).intf1
     c0.setIP(c0_conf["mgmt_ip"], intf=c0_if_mgmt)
@@ -119,7 +146,7 @@ def configure_links(net):
                     addr1=h1_conf["dp_mac"],
                     addr2=dps1_conf["dp_mac_h1"]).intf1
     h1.setIP(h1_conf["dp_ip"], intf=h1_if_dp)
-    h1_if_mgmt = Link(h1, mgmts1, port1=1, port2=1,
+    h1_if_mgmt = Link(h1, mgmts1, port1=1, port2=2,
                       addr1=h1_conf["mgmt_mac"],
                       addr2=mgmts1_conf["mgmt_mac_h1"]).intf1
     h1.setIP(h1_conf["mgmt_ip"], intf=h1_if_mgmt)
@@ -130,10 +157,34 @@ def configure_links(net):
                     addr1=h2_conf["dp_mac"],
                     addr2=dps1_conf["dp_mac_h2"]).intf1
     h2.setIP(h2_conf["dp_ip"], intf=h2_if_dp)
-    h2_if_mgmt = Link(h2, mgmts1, port1=1, port2=2,
+    h2_if_mgmt = Link(h2, mgmts1, port1=1, port2=3,
                       addr1=h2_conf["mgmt_mac"],
                       addr2=mgmts1_conf["mgmt_mac_h2"]).intf1
     h2.setIP(h2_conf["mgmt_ip"], intf=h2_if_mgmt)
+
+
+def _config_mgmts1(net):
+    """Configure the management network switch to behave like a
+    standard layer 2 Ethernet switch.
+
+    :param net: A Mininet object with a virtual network.
+    """
+    # Get the configuration details of the nodes
+    mgmts1_conf, dps1_conf, c0_conf, h1_conf, h2_conf = \
+        get_node_configs()
+
+    # Get the node object for the management network switch
+    mgmts1 = net.getNodeByName(mgmts1_conf["name"])
+
+    # Create a flow table entry instructing the management network
+    # switch to behave like a standard layer 2 Ethernet switch.
+    # Because of the way that Mininet work, our switch with the name
+    # 'mgmts1' is really just an Open vSwitch bridge. This is why
+    # 'mgmts1' is found in bridge interface position in the command.
+    lg.info("*** Disabling OpenFlow and configuring layer 2 Ethernet "
+            "switch behaviour on switch %s\n" % (mgmts1_conf["name"]))
+    mgmts1.cmd("ovs-ofctl add-flow mgmts1 action=normal")
+
 
 
 if __name__ == "__main__":
@@ -141,8 +192,9 @@ if __name__ == "__main__":
     topo = BasicNetwork()
     net = Mininet(topo, build=False, controller=None)
     net.build()
-    configure_links(net)
+    _configure_links(net)
     net.start()
+    _config_mgmts1(net)
     lg.info("*** Dumping host interface connections\n")
     dumpNodeConnections(net.hosts)
     lg.info("*** Dumping switch interface connections\n")
